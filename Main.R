8270a21d4c50a8b164c00970aff17b8866d3763f
Jan-Niklas Rössler <jan-niklas.roessler@fu-berlin.de>
2016-06-06 15:03:07 +0200

    update

--- Main.R
+++ Main.R
@@ -5,18 +5,17 @@ if (kocent){
   pipelineLoc = paste(system("echo $HOME", intern = T), "OptimizeParameters", sep = "/")
 }
 
-pipelineLoc = "/Users/jan-niklas/OptimizeParameters"
+#pipelineLoc = "/Users/jan-niklas/OptimizeParameters"
 setwd(pipelineLoc)
 source("Src/loadPackages.R")
 
 set_ctrl = "Normal"
 set_case = "Disease"
-p_val = 0.05
 
 data_sets = c("GSE1297", "GSE14762", "GSE14924_CD4", "GSE14924_CD8", "GSE15932_Dia", "GSE15932_Panc", "GSE19420", "GSE19728",
               "GSE20153", "GSE20164", "GSE20291", "GSE21354", "GSE24250", "GSE30153", "GSE32676", "GSE3585", "GSE4107", "GSE4183",
 "GSE5281_EC", "GSE5281_HIP", "GSE5281_VCX", "GSE781", "GSE8762", "GSE9348", "GSE9476")
-data_sets = c("GSE1297")#, "GSE14762")
+data_sets = c("GSE14762", "GSE14924_CD4", "GSE14924_CD8", "GSE15932_Dia", "GSE15932_Panc")#, "GSE14762")
 pack_foreach = c("stringr", "hgu133plus2.db", "affy", "simpleaffy", "affyPLM", "affycoretools", "affyQCReport", "annaffy", "limma", "xlsx")
 
 source("Src/parametersToOptimize.R")
@@ -82,12 +81,12 @@ foreach(i = 1:length(data_sets), .packages = pack_foreach) %dopar% {
           source("Src/createPhenoLabels.cls.R", local = TRUE)  
         }
     
-        for(g in 1:length(permutation_type)) %dopar% {
+        for(g in 1:length(permutation_type)){
           permutation = permutation_type[g]
-          for(h in 1:length(local_statistics)) %dopar% {
+          for(h in 1:length(local_statistics)){
             local_statistic = local_statistics[h]
             #### gsea analysis ####
-            source("Src/computeGSEA.R", local = TRUE)
+            source("Src/computeGSEA_java.R", local = TRUE)
           }
         }
       }
