d2801e479adce586f2a9189316c3a331c1d99f21
Jan-Niklas Rössler <jan-niklas.roessler@fu-berlin.de>
2016-06-07 09:53:27 +0200

    update

--- Main.R
+++ Main.R
@@ -15,7 +15,7 @@ set_case = "Disease"
 data_sets = c("GSE1297", "GSE14762", "GSE14924_CD4", "GSE14924_CD8", "GSE15932_Dia", "GSE15932_Panc", "GSE19420", "GSE19728",
               "GSE20153", "GSE20164", "GSE20291", "GSE21354", "GSE24250", "GSE30153", "GSE32676", "GSE3585", "GSE4107", "GSE4183",
 "GSE5281_EC", "GSE5281_HIP", "GSE5281_VCX", "GSE781", "GSE8762", "GSE9348", "GSE9476")
-data_sets = c("GSE14762")  #, "GSE14762")
+data_sets = c("GSE14762", "GSE1297",  "GSE14924_CD4")  #, "GSE14762")
 pack_foreach = c("stringr", "hgu133plus2.db", "affy", "simpleaffy", "affyPLM", "affycoretools", "affyQCReport", "annaffy", "limma", "xlsx")
 
 source("Src/parametersToOptimize.R")
@@ -60,17 +60,19 @@ foreach(i = 1:length(data_sets), .packages = pack_foreach) %dopar% {
         #### annotate data ####
         source("Src/annotation.R", local = TRUE)
     
-        for(p in 1:length(p_change)){
-          p_val = p_change[p]
-          for(l in 1:length(lfc_change)){
-            lfc_exp = lfc_change[l]
-            #### limma analysis ####
-            source("Src/computeLimma.R", local = TRUE)
+        ####
+        source("Src/computeLimma.R", local = TRUE)
+        #for(p in 1:length(p_change)){
+        #  p_val = p_change[p]
+        #  for(l in 1:length(lfc_change)){
+        #    lfc_exp = lfc_change[l]
+        #    #### limma analysis ####
+        #    source("Src/setCutoff_Limma.R", local = TRUE)
   
-            #### limma gene set analysis ####
-            source("Src/limmaGSOA.R", local = TRUE)
-          }
-        }
+        #    #### limma gene set analysis ####
+        #    source("Src/limmaGSOA.R", local = TRUE)
+        #  }
+        #}
         
         #### create expressionSet.gct if doesn't exist ####
         if(! file.exists(paste(celFilesPath, paste("ExpressionSet_", paste(backMethod, normalizeMethod, summaryMethod, sep = "_"), ".gct", sep =""), sep = "/"))){
@@ -83,24 +85,24 @@ foreach(i = 1:length(data_sets), .packages = pack_foreach) %dopar% {
           source("Src/createPhenoLabels.cls.R", local = TRUE)  
         }
     
-        for(g in 1:length(permutation_type)){
-          permutation = permutation_type[g]
-          for(h in 1:length(local_statistics)){
-            local_statistic = local_statistics[h]
-            #### gsea analysis ####
-            source("Src/computeGSEA_java.R", local = TRUE)
-          }
-        }
-      }
-    }
-  }
+        #for(g in 1:length(permutation_type)){
+        #  permutation = permutation_type[g]
+        #  for(h in 1:length(local_statistics)){
+        #    local_statistic = local_statistics[h]
+        #    #### gsea analysis ####
+        #    source("Src/computeGSEA_java.R", local = TRUE)
+        #  }
+        #}
+      #}
+    #}
+  #}
   #### performance limma - extract ranks, p-values, q-values
-  source("Src/extractResults_limma.R", local = TRUE)
+  #source("Src/extractResults_limma.R", local = TRUE)
   
   #### performance gsea - extract ranks, p-values, q-values
-  source("Src/extractResults_gsea.R", local = TRUE)
+  #source("Src/extractResults_gsea.R", local = TRUE)
   
   #### write final results
-  source("Src/writeFinalResults.R", local = TRUE)
+  #source("Src/writeFinalResults.R", local = TRUE)
 }
 stopCluster(clusters)
--- Src/createExpression.gct.R
+++ Src/createExpression.gct.R
@@ -9,14 +9,16 @@ if(length(names_dupli) > 0){
   for (i in 1:length(names_dupli)){
     index = which(expression_out$NAMES == names_dupli[i])
     current = expression_out[index, ]
-    exprs_case_gsea = rowMeans(current[, index_case_gsea ])
-    exprs_ctrl_gsea = rowMeans(current[, index_ctrl_gsea ])
-    dif_exp_gsea    = exprs_case_gsea - exprs_ctrl_gsea
-    index_max = which.max(abs(dif_exp_gsea))
-    temp      = as.data.frame(current[index_max,])
-    reduced   = c(reduced, list(temp))
+    current = aggregate(. ~ NAMES, data = current, median)
+    #exprs_case_gsea = rowMeans(current[, index_case_gsea ])
+    #exprs_ctrl_gsea = rowMeans(current[, index_ctrl_gsea ])
+    #dif_exp_gsea    = exprs_case_gsea - exprs_ctrl_gsea
+    #index_max = which.max(abs(dif_exp_gsea))
+    #temp      = as.data.frame(current[index_max,])
+    reduced   = c(reduced, list(current))
   }
   reduced = do.call("rbind", reduced)
+  reduced$DESCRIPTION = rep("NA", length(reduced$DESCRIPTION))
   expression_out = expression_out[- index_dupli, ]
   expression_out = rbind(expression_out, reduced)
 }
