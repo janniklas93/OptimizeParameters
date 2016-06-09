a7ff1f57b82e23b59fcb34a5adebe9078ba1f702
root <root@kocent.local>
2016-06-09 12:25:57 +0200

    update

--- Main.R
+++ Main.R
@@ -15,14 +15,14 @@ set_case = "Disease"
 data_sets = c("GSE1297", "GSE14762", "GSE14924_CD4", "GSE14924_CD8", "GSE15932_Dia", "GSE15932_Panc", "GSE19420", "GSE19728",
               "GSE20153", "GSE20164", "GSE20291", "GSE21354", "GSE24250", "GSE30153", "GSE32676", "GSE3585", "GSE4107", "GSE4183",
 "GSE5281_EC", "GSE5281_HIP", "GSE5281_VCX", "GSE781", "GSE8762", "GSE9348", "GSE9476")
-data_sets = c("GSE14924_CD8")
-pack_foreach = c("stringr", "hgu133plus2.db", "affy", "simpleaffy", "affyPLM", "affycoretools", "affyQCReport", "annaffy", "limma", "xlsx")
+data_sets = c("GSE20164")
+#pack_foreach = c("stringr", "hgu133plus2.db", "affy", "simpleaffy", "affyPLM", "affycoretools", "affyQCReport", "annaffy", "limma", "xlsx")
 
 source("Src/parametersToOptimize.R")
 
-numCores = detectCores() - 1
-clusters = makeCluster(numCores)
-registerDoMC(numCores)
+#numCores = detectCores() - 1
+#clusters = makeCluster(numCores)
+#registerDoMC(numCores)
 #clusterExport(clusters, c("data_sets", "pipelineLoc", "set_ctrl", "set_case", "p_val", "lfc_exp"))
 #registerDoParallel(clusters)
 
@@ -30,7 +30,7 @@ source("Src/processGeneSetDB.R")
 
 target_pathways = read.table(paste(pipelineLoc, "/Misc/target_pathways.tab", sep = "/"), sep = "\t", header = TRUE)
 
-foreach(i = 1:length(data_sets), .packages = pack_foreach) %dopar% {
+for(i in 1:length(data_sets)) {
 
   dataSet = data_sets[i]
   #### set generic initial parameters ####
@@ -61,18 +61,18 @@ foreach(i = 1:length(data_sets), .packages = pack_foreach) %dopar% {
         source("Src/annotation.R", local = TRUE)
     
         ####
-        #source("Src/computeLimma.R", local = TRUE)
-        #for(p in 1:length(p_change)){
-        #  p_val = p_change[p]
-        #  for(l in 1:length(lfc_change)){
-        #    lfc_exp = lfc_change[l]
-        #    #### limma analysis ####
-        #    source("Src/setCutoff_Limma.R", local = TRUE)
+        source("Src/computeLimma.R", local = TRUE)
+        for(p in 1:length(p_change)){
+          p_val = p_change[p]
+          for(l in 1:length(lfc_change)){
+            lfc_exp = lfc_change[l]
+            #### limma analysis ####
+            source("Src/setCutoff_Limma.R", local = TRUE)
   
-        #    #### limma gene set analysis ####
-        #    source("Src/limmaGSOA.R", local = TRUE)
-        #  }
-        #}
+            #### limma gene set analysis ####
+            source("Src/limmaGSOA.R", local = TRUE)
+          }
+        }
         
         #### create expressionSet.gct if doesn't exist ####
         if(! file.exists(paste(celFilesPath, paste("ExpressionSet_", paste(backMethod, normalizeMethod, summaryMethod, sep = "_"), ".gct", sep =""), sep = "/"))){
@@ -97,7 +97,7 @@ foreach(i = 1:length(data_sets), .packages = pack_foreach) %dopar% {
     }
   }
   #### performance limma - extract ranks, p-values, q-values
-  #source("Src/extractResults_limma.R", local = TRUE)
+  source("Src/extractResults_limma.R", local = TRUE)
   
   #### performance gsea - extract ranks, p-values, q-values
   #source("Src/extractResults_gsea.R", local = TRUE)
@@ -105,4 +105,4 @@ foreach(i = 1:length(data_sets), .packages = pack_foreach) %dopar% {
   #### write final results
   #source("Src/writeFinalResults.R", local = TRUE)
 }
-stopCluster(clusters)
+#stopCluster(clusters)
