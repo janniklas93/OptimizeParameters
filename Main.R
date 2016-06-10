30b08952419f870bf6b2d6e70a4785994e3c57f8
Jan-Niklas Rössler <jan-niklas.roessler@fu-berlin.de>
2016-06-10 09:59:17 +0200

    update

--- Main.R
+++ Main.R
@@ -15,7 +15,7 @@ set_case = "Disease"
 data_sets = c("GSE1297", "GSE14762", "GSE14924_CD4", "GSE14924_CD8", "GSE15932_Dia", "GSE15932_Panc", "GSE19420", "GSE19728",
               "GSE20153", "GSE20164", "GSE20291", "GSE21354", "GSE24250", "GSE30153", "GSE32676", "GSE3585", "GSE4107", "GSE4183",
 "GSE5281_EC", "GSE5281_HIP", "GSE5281_VCX", "GSE781", "GSE8762", "GSE9348", "GSE9476")
-data_sets = c("GSE20291", "GSE21354", "GSE24250")
+data_sets = c("GSE32676")#, "GSE21354", "GSE24250")
 #pack_foreach = c("stringr", "hgu133plus2.db", "affy", "simpleaffy", "affyPLM", "affycoretools", "affyQCReport", "annaffy", "limma", "xlsx")
 
 source("Src/parametersToOptimize.R")
@@ -30,11 +30,8 @@ source("Src/processGeneSetDB.R")
 
 target_pathways = read.table(paste(pipelineLoc, "/Misc/target_pathways.tab", sep = "/"), sep = "\t", header = TRUE)
 
-<<<<<<< HEAD
-for (i in 1:length(data_sets)){
-=======
 for(i in 1:length(data_sets)) {
->>>>>>> e920b843fc779afc0d4174224ab820514d07e312
+
 
   dataSet = data_sets[i]
   #### set generic initial parameters ####
@@ -89,14 +86,14 @@ for(i in 1:length(data_sets)) {
           source("Src/createPhenoLabels.cls.R", local = TRUE)  
         }
     
-        #for(g in 1:length(permutation_type)){
-        #  permutation = permutation_type[g]
-        #  for(h in 1:length(local_statistics)){
-        #    local_statistic = local_statistics[h]
-        #    #### gsea analysis ####
-        #    source("Src/computeGSEA_java.R", local = TRUE)
-        #  }
-        #}
+        for(g in 1:length(permutation_type)){
+          permutation = permutation_type[g]
+          for(h in 1:length(local_statistics)){
+            local_statistic = local_statistics[h]
+            #### gsea analysis ####
+            source("Src/computeGSEA_java.R", local = TRUE)
+          }
+        }
       }
     }
   }
@@ -104,13 +101,9 @@ for(i in 1:length(data_sets)) {
   source("Src/extractResults_limma.R", local = TRUE)
   
   #### performance gsea - extract ranks, p-values, q-values
-  #source("Src/extractResults_gsea.R", local = TRUE)
+  source("Src/extractResults_gsea.R", local = TRUE)
   
   #### write final results
-  #source("Src/writeFinalResults.R", local = TRUE)
+  source("Src/writeFinalResults.R", local = TRUE)
 }
-<<<<<<< HEAD
 #stopCluster(clusters)
-=======
-#stopCluster(clusters)
->>>>>>> e920b843fc779afc0d4174224ab820514d07e312
