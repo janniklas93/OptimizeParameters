22580962f05454a579463e9077ad36f534372ad3
Jan-Niklas Rössler <jan-niklas.roessler@fu-berlin.de>
2016-06-13 13:18:48 +0200

    update

--- Main.R
+++ Main.R
@@ -15,7 +15,7 @@ set_case = "Disease"
 data_sets = c("GSE1297", "GSE14762", "GSE14924_CD4", "GSE14924_CD8", "GSE15932_Dia", "GSE15932_Panc", "GSE19420", "GSE19728",
               "GSE20153", "GSE20164", "GSE20291", "GSE21354", "GSE24250", "GSE30153", "GSE32676", "GSE3585", "GSE4107", "GSE4183",
 "GSE5281_EC", "GSE5281_HIP", "GSE5281_VCX", "GSE781", "GSE8762", "GSE9348", "GSE9476")
-data_sets = c("GSE9348")
+data_sets = c("GSE4183")
 #pack_foreach = c("stringr", "hgu133plus2.db", "affy", "simpleaffy", "affyPLM", "affycoretools", "affyQCReport", "annaffy", "limma", "xlsx")
 
 source("Src/parametersToOptimize.R")
